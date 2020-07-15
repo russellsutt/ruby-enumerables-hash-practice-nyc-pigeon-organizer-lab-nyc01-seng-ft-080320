2f4040664a75ad8e9e700ad485dcdc906674f6b6
Russell Sutter <russell.sutter@gmail.com>
2020-07-15 17:57:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,20 +20,20 @@ pigeon_data = {
 }
 
 
-
 def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), array|
     value.each do |inner_key, names|
       names.each do |name|
-        if !array[name]
-          array[name] = {}
+        if array.include?(name)
+        else array[name] = {}
         end
-        if !array[key]
-          array[name][key] = []
+        if array.include?(key)
+        else array[name][key] = []
         end
-      array[name][key].push(inner_key.to_s)
       end
+      array[name][key].push(inner_key.to_s)
     end
   end
 end
+
 
